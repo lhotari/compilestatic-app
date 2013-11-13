aa71ff378d224cbaf9e42d30dc57663436175582
Lari Hotari <lari.hotari@sagire.fi>
2013-11-13 18:59:22 +0200

    Add more complex examples

--- grails-app/controllers/MyController.groovy
+++ grails-app/controllers/MyController.groovy
@@ -1,8 +1,9 @@
 @groovy.transform.CompileStatic
 class MyController {
 
-    def index() {
-        render text:Person.list().collect{Person p -> p.firstName}.toString()
+    
+    def index() { 
+        render(text:(Person.get(1).firstName + " " + Person.list().collect{Person p -> p.firstName}.toString()))
     }
 
     def add(Person p) {
