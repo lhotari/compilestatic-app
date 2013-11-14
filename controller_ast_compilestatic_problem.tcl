1dba82624f72019096365e381d4acbb3301515f2
Lari Hotari <lari.hotari@sagire.fi>
2013-11-14 07:13:42 +0200

    refactor script: extract method

--- controller_ast_compilestatic_problem.tcl
+++ controller_ast_compilestatic_problem.tcl
@@ -4,6 +4,9 @@
 # shows error [Static type checking] - Cannot find matching method 
 # MyController#render(java.util.LinkedHashMap <java.lang.String, java.lang.String>). 
 # Please check if the declared type is right and if the method exists.
+proc loadurl {} {
+  system curl -sS -i http://localhost:8080/compilestatic-app/my/index    	
+}
 set timeout 60
 exec git checkout f740e65bc grails-app/controllers/MyController.groovy
 spawn bash --norc --noprofile
@@ -18,7 +21,7 @@ puts "\nGrails started. Waiting 10 seconds..."
 sleep 10
 set timeout 10
 puts "loading url...\n"
-system curl -sS -i http://localhost:8080/compilestatic-app/my/index
+loadurl
 puts "\nwaiting 10 seconds...\n"
 sleep 10
 puts "changing MyController.groovy\n"
@@ -27,10 +30,10 @@ expect "\n"
 puts "waiting 10 seconds...\n"
 sleep 10
 puts "loading url...\n"
-system curl -sS -i http://localhost:8080/compilestatic-app/my/index
+loadurl
 sleep 5
 puts "\nloading url 2nd time...\n"
-system curl -sS -i http://localhost:8080/compilestatic-app/my/index
+loadurl
 send "exit\r"
 expect "$ "
 exec git checkout HEAD grails-app/controllers/MyController.groovy
