c00e0b3ed0597928672807e8fe67d2133f4fdd14
Lari Hotari <lari.hotari@sagire.fi>
2013-11-14 07:07:57 +0200

    curl output to stdout

--- controller_ast_compilestatic_problem.tcl
+++ controller_ast_compilestatic_problem.tcl
@@ -14,18 +14,23 @@ send "grails -plain-output\r"
 expect "grails>"
 send "run-app\r"
 expect "grails>"
+puts "\nGrails started. Waiting 10 seconds..."
 sleep 10
 set timeout 10
-puts "loading url..."
-exec curl -sS -i http://localhost:8080/compilestatic-app/my/index
-expect "\n"
-puts "changing MyController.groovy"
+puts "loading url...\n"
+system curl -sS -i http://localhost:8080/compilestatic-app/my/index
+puts "\nwaiting 10 seconds...\n"
+sleep 10
+puts "changing MyController.groovy\n"
 exec git checkout aa71ff37 grails-app/controllers/MyController.groovy
 expect "\n"
-puts "loading url..."
-exec curl -sS -i http://localhost:8080/compilestatic-app/my/index
-expect "\n"
+puts "waiting 10 seconds...\n"
+sleep 10
+puts "loading url...\n"
+system curl -sS -i http://localhost:8080/compilestatic-app/my/index
 sleep 5
+puts "\nloading url 2nd time...\n"
+system curl -sS -i http://localhost:8080/compilestatic-app/my/index
 send "exit\r"
 expect "$ "
 exec git checkout HEAD grails-app/controllers/MyController.groovy
